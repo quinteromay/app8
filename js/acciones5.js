c3fa390b37d20b046648acc4b0e7eaa5c6f9459b
aryit <38222680+aryit@users.noreply.github.com>
2018-05-25 08:00:24 +0200

    subida29

--- js/acciones5.js
+++ js/acciones5.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 $ (document).ready(function (e) {
 	document.addEventListener("deviceready", function() {
-		$('#beep').tap(function() {
+		$('#sonar').tap(function() {
 			navigator.notification.beep(2);
 		}); //cierre del beep
 		$('#vibrar').tap(function() {
