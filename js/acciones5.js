ec79ea1acb80ffbb6859767619e7ad91549d8b1a
aryit <38222680+aryit@users.noreply.github.com>
2018-05-25 07:51:54 +0200

    subida29

--- js/acciones5.js
+++ js/acciones5.js
@@ -1,11 +1,11 @@
 // JavaScript Document
-$(document).ready(function(e){
-	document.addEventListener("deviceready", function(){
-		$('#sonar').tap(function(){
+$ (document).ready(function (e) {
+	document.addEventListener("deviceready", function() {
+		$('#beep').tap(function() {
 			navigator.notification.beep(2);
 		}); //cierre del beep
-		$('#vibrar').tap(function(){
+		$('#vibrar').tap(function() {
 			navigator.notification.vibrate(2000);
-		}); //cierre del vibrar
+		});//cierre del vibrar
 	},false); //cierre del deviceready
-}); // cierre ready 
+}); // cierre ready
--- js/acciones7.js
+++ js/acciones7.js
@@ -2,22 +2,22 @@
 
 $(document).ready(function(e) {
     document.addEventListener("deviceready",function(){
-		$('#izquierda').on("swipeleft", function(){
-			navigator.notification.alert("deslizó hacia la izquierda",function(){"aplicacion 7","Ok"});
-		});//cerrar deslizar izquierda
+	$('#izquierda').on("swipeleft", function(){
+		navigator.notification.alert("Deslizó hacia la izquierda", function(){"aplicacion7","Ok"});
+		});//cerrar deslizar hacia la izquierda
 		$('#derecha').on("swiperight",function(){
-			navigator.notification.confirm("¿Que quieres hacer?",function(op){
+			navigator.notification.confirm("¿Qué quieres hacer?",function(op){
 				switch(op)
 				{
 					case 1:
 					navigator.notification.beep(1);
 					break;
-					
 					case 2:
 					navigator.notification.vibrate(3000);
 					break;
-				}
-			},"aplicacion 7","Beepear,Vibrar,Cancelar");
-		});//cerrar deslizar derecha
-	},false);//cerrar deviceready
+					
+				 }
+			},"aplicacion 7","beepear,vibrate,cancelar");
+		});//cerrar deslizar hacia derecha
+	},false);//cerrer deviceready
 });//cerrar document
